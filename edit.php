0e53ba532973fb40efd98e97a15433525a99f007
dimmascool <dimasadam058@gmail.com>
2023-10-27 15:30:37 +0700

    update

--- edit.php
+++ edit.php
@@ -2,20 +2,25 @@
 
 require_once 'koneksi.php';
 
+$id = $_GET['mhs'];
+
 if (isset($_POST['submit'])) {
     $nama = $_POST['nama'];
     $nim = $_POST['nim'];
     $kelas = $_POST['kelas'];
 
-    $submit_db = mysqli_query($koneksi, "INSERT INTO data_mhs (`nim`, `nama`, `kelas`) VALUES ('$nim', '$nama', '$kelas')");
+    $submit_db = mysqli_query($koneksi, "UPDATE data_mhs SET nama ='$nama', nim ='$nim',kelas ='$kelas' WHERE id = '$id'");
 
     if ($submit_db) {
-        echo "<script>alert('berhasil menyimpan data');location.href='read.php'</script>";
+        echo "<script>alert('berhasil mengubah data');location.href='read.php'</script>";
     } else {
-        echo "<script>alert('gagal menyimpan data');</script>";
+        echo "<script>alert('gagal mengubah data');</script>";
         echo mysqli_error($koneksi);
     }
 }
+
+$data_mhs = mysqli_query($koneksi, "SELECT * FROM data_mhs WHERE id = '$id'");
+$mhs = mysqli_fetch_assoc($data_mhs);
 ?>
 
 <!DOCTYPE html>
@@ -30,11 +35,11 @@ if (isset($_POST['submit'])) {
 <body>
     <form action="" method="POST">
         <label for="">NIM</label>
-        <input type="text" name="nim">
+        <input type="text" name="nim" value="<?= $mhs['nim'] ?>">
         <label for="">Nama</label>
-        <input type="text" name="nama">
+        <input type="text" name="nama" value="<?= $mhs['nama'] ?>">
         <label for="">Kelas</label>
-        <input type="text" name="kelas">
+        <input type="text" name="kelas" value="<?= $mhs['kelas'] ?>">
         <input type="submit" name="submit">
     </form>
 </body>
--- read.php
+++ read.php
@@ -2,21 +2,72 @@
 
 require_once 'koneksi.php';
 
+if (isset($_GET['delete_user'])) {
+    $user_id = $_GET['delete_user'];
+
+    $do_delete = mysqli_query($koneksi, "DELETE FROM data_mhs WHERE id = '$user_id'");
+
+    if ($do_delete) {
+        echo "<script>alert('berhasil menghapus data');location.href='read.php'</script>";
+    } else {
+        echo "<script>alert('gagal menghapus data');</script>";
+        echo mysqli_error($koneksi);
+    }
+}
+
 $data_mhs = mysqli_query($koneksi, "SELECT * FROM data_mhs");
 
 ?>
 
-<table border="1" style="width: 100%;">
-    <tr>
-        <th>NIM</th>
-        <th>Nama</th>
-        <th>Kelas</th>
-    </tr>
-    <?php foreach ($data_mhs as $mhs) : ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+    :root {
+        --blue: #1e90ff;
+        --white: #ffffff;
+        --black: #000;
+    }
+
+    .tambahData {
+        background-color: red;
+    }
+
+    #tambahData {
+        color: var(--white);
+    }
+</style>
+
+<body>
+    <hr>
+    <a href="create.php">Tambah data</a>
+    <hr>
+
+    <table border="1" style="width: 100%;">
         <tr>
-            <td><?= $mhs['nim'] ?></td>
-            <td><?= $mhs['nama'] ?></td>
-            <td><?= $mhs['kelas'] ?></td>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Kelas</th>
+            <th>Opsi</th>
         </tr>
-    <?php endforeach ?>
-</table>
+        <?php foreach ($data_mhs as $mhs) : ?>
+            <tr>
+                <td><?= $mhs['nim'] ?></td>
+                <td><?= $mhs['nama'] ?></td>
+                <td><?= $mhs['kelas'] ?></td>
+                <td>
+                    <a href="edit.php?mhs=<?= $mhs['id'] ?>">Edit</a> ||
+                    <a href="?delete_user=<?= $mhs['id'] ?>">Hapus</a>
+                </td>
+            </tr>
+        <?php endforeach ?>
+    </table>
+</body>
+
+</html>
